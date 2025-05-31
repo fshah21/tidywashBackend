ad37768c62d9268d90f668fcac573ca080d9e526
Feya Shah <feyashah21@gmail.com>
2025-05-31 19:10:44 +0530

    get endpoint

--- userservice/src/routes/employee.routes.ts
+++ userservice/src/routes/employee.routes.ts
@@ -4,4 +4,4 @@ import { EmployeeController } from "../controllers/employee.controller";
 export const employeeRoutes = Router();
 
 employeeRoutes.post("/createEmployee", EmployeeController.createEmployee);
-employeeRoutes.post("/getAllEmployees", EmployeeController.getAllEmployees);
+employeeRoutes.get("/getAllEmployees", EmployeeController.getAllEmployees);
