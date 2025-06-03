ce0bde2c2fbbddb4eed73ca7cf38c937d074a890
Feya Shah <feyashah21@gmail.com>
2025-06-03 21:57:09 +0530

    fixing query

--- orderservice/src/controllers/order.controller.ts
+++ orderservice/src/controllers/order.controller.ts
@@ -193,7 +193,7 @@ export class OrderController {
         const orders = await Order.findAll({
           where: {
             status: {
-              [Op.ne]: ["completed", "active"]
+              [Op.notIn]: ["completed", "active"]
             }
           }
         });
