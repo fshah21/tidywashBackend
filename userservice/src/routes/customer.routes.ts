2bc6d1e67d5439440fd81e6a12d00811282949c1
Feya Shah <feyashah21@gmail.com>
2025-06-20 21:14:34 +0530

    get route

--- userservice/src/routes/customer.routes.ts
+++ userservice/src/routes/customer.routes.ts
@@ -5,5 +5,5 @@ export const customerRoutes = Router();
 
 customerRoutes.get("/getAddressesForCustomer/:customer_id", CustomerController.getAddressesForCustomer);
 customerRoutes.post("/addAddress", CustomerController.addAddress);
-customerRoutes.post("/getAddressById/:address_id", CustomerController.getAddressById);
+customerRoutes.get("/getAddressById/:address_id", CustomerController.getAddressById);
 customerRoutes.get("/getCustomerById/:customer_id", CustomerController.getCustomerById);
